8928851d53a439ebc90a2607126602ebcc203330
samuel <samuelkelvinmwangi@gmail.com>
2023-07-04 09:16:18 +0300

    assingnment on attribute accesors

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name
+  
+    def meow
+      puts "meow!"
+    end
+  end
+  
+my_cat = Cat.new
+my_cat.name = "sam"
+puts my_cat.name  
+my_cat.meow  
